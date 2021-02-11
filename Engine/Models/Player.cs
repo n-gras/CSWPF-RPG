b1d5689faccc2295dd1f3b0e9fbe0d605883df6b
n-gras <niels.gras@outlook.com>
2021-02-11 15:51:52 +0100

    Refactor Player, Monster and Trader props to LivingEntity

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,33 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
 
 namespace Engine.Models
 {
-    public class Player : BaseNotificationClass
+    public class Player : LivingEntity
     {
         #region Properties
 
-        private string _name;
         private string _characterClass;
-        private int _hitPoints;
         private int _experiencePoints;
         private int _level;
-        private int _gold;
 
-        public string Name 
-        {
-            get { return _name; }
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            } 
-        }
         public string CharacterClass
         {
             get { return _characterClass; }
@@ -37,15 +21,7 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(CharacterClass));
             }
         }
-        public int HitPoints
-        {
-            get { return _hitPoints; }
-            set
-            {
-                _hitPoints = value;
-                OnPropertyChanged(nameof(HitPoints));
-            }
-        }
+
         public int ExperiencePoints 
         {
             get { return _experiencePoints; }
@@ -64,20 +40,7 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(Level));
             }
         }
-        public int Gold
-        {
-            get { return _gold; }
-            set
-            {
-                _gold = value;
-                OnPropertyChanged(nameof(Gold));
-            }
-        }
 
-        public ObservableCollection<GameItem> Inventory { get; set; }
-        // observable collection handles notifications automatically
-
-        public List<GameItem> Weapons => Inventory.Where(i => i is Weapon).ToList();
 
         public ObservableCollection<QuestStatus> Quests { get; set; }
 
@@ -85,22 +48,7 @@ namespace Engine.Models
 
         public Player()
         {
-            Inventory = new ObservableCollection<GameItem>();
             Quests = new ObservableCollection<QuestStatus>();
-        }
-
-        public void AddItemToInventory(GameItem item)
-        {
-            Inventory.Add(item);
-
-            OnPropertyChanged(nameof(Weapons));
-        }
-
-        public void RemoveItemFromInventory(GameItem item)
-        {
-            Inventory.Remove(item);
-
-            OnPropertyChanged(nameof(Weapons));
         }
 
         public bool HasAllTheseItems(List<ItemQuantity> items)
