a6ea088becd075e40fd32d596cf0b0730be9581f
n-gras <niels.gras@outlook.com>
2021-02-15 12:13:35 +0100

    Moved Weapon class into GameItem class.

--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -70,7 +70,8 @@ namespace Engine.Models
 
         public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; }
 
-        public List<GameItem> Weapons => Inventory.Where(i => i is Weapon).ToList();
+        public List<GameItem> Weapons => 
+            Inventory.Where(i => i.Category == GameItem.ItemCategory.Weapon).ToList();
 
         public bool IsDead => CurrentHitPoints <= 0; // expression-body property, returns result of computation
 
